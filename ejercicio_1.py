52183c218d014e910ab492b410f23b7c29507cf8
jvago10 <jose.vargas@intertech.network>
2024-07-14 12:31:53 -0500

    Se completa el ejercicio 1 y se comenta algunos tips

--- ejercicio_1.py
+++ ejercicio_1.py
@@ -5,5 +5,16 @@ num1 = int(input("Ingrese el numero 1 entero: "))
 num2 = int(input("Ingrese el numero 2 entero: "))
 
 suma = int(num1 + num2)
+resta = int(num1 - num2)
+multiplicacion = int(num1*num2)
+division = round(num1/num2, 3)#acepta 3 digitos para la division
+divison_entera = round(num1 / num2,)#acepta 2 digitos para la division
+residuo = num1 % num2
 
-print (f' la suma es: {suma}')
+print (f'La suma es: {suma}')
+print (f'La resta es: {resta}')
+print (f'La multiplicación es: {multiplicacion}')
+print (f'la división es: {division}')
+print (f'La division entera es: {divison_entera}')
+print (f'El residuo es: {residuo}')
+
