746e0d7d4ea0f9abdcc914549804f91cba382dee
jvago10 <jose.vargas@intertech.network>
2024-07-23 11:38:31 -0500

    desarrollo en clase

---
+++ ejercicio_6.py
@@ -0,0 +1,2 @@
+#Escribe un programa que solicite al usuario tres números y determine
+#cuál de ellos es el mayor.
---
+++ ejercicio_7.py
@@ -0,0 +1,11 @@
+#Escribe un programa que pida al usuario un numero entero
+#y muestre por pantalla un triángulO rectángulo
+#Como el del mas abajo
+#De altura el numero introducido.
+
+altura = int(input("Introduce el número entero: "))
+
+for i in range(1,altura+1):
+    print ('*'*i)
+             
+             
