65d64510cb3f5d4803ab31800926433e253e9714
José Vargas Gonzales <jose.vargas@intertech.network>
2024-08-04 13:25:32 -0500

    mejores practicas

--- EJERCICIOS_CLASES/EJERCICIOS_YOUTUBE/EJERCICIO_SOY_DALTO/ARCHIVOS/leer_txt_optimamente.py
+++ EJERCICIOS_CLASES/EJERCICIOS_YOUTUBE/EJERCICIO_SOY_DALTO/ARCHIVOS/leer_txt_optimamente.py
@@ -1,6 +1,15 @@
+#Abriendo el archivo con with open 
+
 with open("C:\PROYECTOS\EJERCICIOS_CLASES\EJERCICIOS_YOUTUBE\EJERCICIO_SOY_DALTO\ARCHIVOS\\texto_de_jose.txt", encoding="UTF-8") as archivo:
-    print(archivo.read())
     
+    #leemos el contenido
+    contenido = archivo.read()
+    
+    #mostramos el archivo
+    
+    print(contenido)
+
+#No es necesario cerrarlo al usar with open    
     
     
 
