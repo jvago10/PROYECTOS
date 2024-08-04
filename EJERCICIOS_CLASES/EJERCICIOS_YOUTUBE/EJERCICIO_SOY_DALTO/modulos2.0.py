ff2eed6f25612250bbb3cc0ff4c806a7e7fb29ff
José Vargas Gonzales <jose.vargas@intertech.network>
2024-08-04 11:07:26 -0500

    se actualiza los archivos, con modulos y paquetes

---
+++ EJERCICIOS_CLASES/EJERCICIOS_YOUTUBE/EJERCICIO_SOY_DALTO/modulos2.0.py
@@ -0,0 +1,15 @@
+import sys 
+
+sys.path.append("C:\PROYECTOS\EJERCICIOS_CLASES\EJERCICIOS_YOUTUBE\EJERCICIO_SOY_DALTO")
+
+print(sys.path)
+
+import saludar as salud_mod
+
+print(salud_mod.saludar("José"))
+
+
+ 
+ 
+      
+      
