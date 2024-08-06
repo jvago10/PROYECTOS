7289d641be63a0d2efe52656282274c6dbb3973f
José Vargas Gonzales <jose.vargas@intertech.network>
2024-08-06 12:19:06 -0500

    trabajando con archivos

---
+++ EJERCICIOS_CLASES/EJERCICIOS_YOUTUBE/EJERCICIO_SOY_DALTO/ARCHIVOS/escribir_txt.py
@@ -0,0 +1,6 @@
+with open ('C:\PROYECTOS\EJERCICIOS_CLASES\EJERCICIOS_YOUTUBE\EJERCICIO_SOY_DALTO\ARCHIVOS\\texto_de_jose.txt', "w", encoding="UTF-8") as archivo:
+    
+    #Sobreescribir el archivo
+    #archivo.write("Hola, soy Jose. Este es mi primer texto.")
+    
+    archivo.writelines(["Hola chaval cómo andas", ", mucho tiempo sin verte"])
