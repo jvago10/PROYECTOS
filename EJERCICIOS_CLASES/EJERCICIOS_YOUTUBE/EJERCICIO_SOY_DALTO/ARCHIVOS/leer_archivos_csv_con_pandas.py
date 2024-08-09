8660bdf06366e18fc5a296e286d6d9b9caef0194
José Vargas Gonzales <jose.vargas@intertech.network>
2024-08-09 08:52:37 -0500

    se complementa info

---
+++ EJERCICIOS_CLASES/EJERCICIOS_YOUTUBE/EJERCICIO_SOY_DALTO/ARCHIVOS/leer_archivos_csv_con_pandas.py
@@ -0,0 +1,15 @@
+import pandas as pd
+#usando la función read_csv para leer el archivo csv
+df = pd.read_csv('C:\PROYECTOS\EJERCICIOS_CLASES\EJERCICIOS_YOUTUBE\EJERCICIO_SOY_DALTO\ARCHIVOS\\datos.csv')
+
+#obtieniendo los datos de la columna nombre
+nombres =  (df["nombre"])
+
+#recorrer por cadena
+
+cadena = "0123456789"
+
+obtener_por_elementos = (cadena[2:6])
+
+
+
